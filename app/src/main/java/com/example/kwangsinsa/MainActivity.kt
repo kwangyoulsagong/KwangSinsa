8a393e530dd254bfff7fd87930ee1a9632795bc1
Kwangyoul <sgky0511@naver.com>
2023-01-27 22:55:00 +0900

    frontend

--- app/src/main/java/com/example/kwangsinsa/MainActivity.kt
+++ app/src/main/java/com/example/kwangsinsa/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.kwangsinsa
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Button
-import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewPager: ViewPager
@@ -20,17 +23,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewPager=findViewById(R.id.container)
         viewPager.adapter=slideradapter(this)
-
-
-
+        var btnarray= arrayOf<Button>(
+            findViewById(R.id.button8),
+            findViewById(R.id.button9),
+            findViewById(R.id.button10),
+            findViewById(R.id.button11))
+        var fragmentarray= arrayOf<Fragment>(recommend(),rank())
+        btnarray[0].paint!!.isUnderlineText=true
         val thread =Thread(NextPage())
         thread.start()
-        findViewById<Button>(R.id.button9).setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.one,rank())
-                addToBackStack(null)
-                commit()
+        for(i in btnarray.indices){
+            btnarray[i].setOnClickListener{
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.one, fragmentarray[i])
+                    addToBackStack(null)
+                    commit()
+                    for(j in btnarray.indices){
+                        btnarray[j].setTextColor(Color.parseColor("#BDB7B7"))
+                        btnarray[j].paint!!.isUnderlineText=false
+                    }
+                    btnarray[i].setTextColor(Color.parseColor("#070707"))
+                    btnarray[i].paint!!.isUnderlineText=true
+
+
+                }
             }
+
         }
     }
     fun setslide(){
--- app/src/main/java/com/example/kwangsinsa/rank.kt
+++ app/src/main/java/com/example/kwangsinsa/rank.kt
@@ -5,14 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
 class rank : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        var rootview=inflater.inflate(R.layout.fragment_rank, container, false)
+        var imagearr= arrayOf(
+            R.drawable.o,
+            R.drawable.e,
+            R.drawable.r,
+            R.drawable.q,
+            R.drawable.u,
+            R.drawable.d,
+            R.drawable.a,
+            R.drawable.l,
 
-        return inflater.inflate(R.layout.fragment_rank, container, false)
+        )
+        var numbertextarr= arrayOf<TextView>(
+            rootview.findViewById(R.id.textView16),
+            rootview.findViewById(R.id.textView17),
+            rootview.findViewById(R.id.textView18),
+            rootview.findViewById(R.id.textView19),
+            rootview.findViewById(R.id.textView20),
+            rootview.findViewById(R.id.textView38),
+            rootview.findViewById(R.id.textView39),
+            rootview.findViewById(R.id.textView40),
+
+        )
+        var imageviewarr = arrayOf(
+            rootview.findViewById<ImageView>(R.id.imageView2),
+            rootview.findViewById<ImageView>(R.id.imageView3),
+            rootview.findViewById<ImageView>(R.id.imageView4),
+            rootview.findViewById<ImageView>(R.id.imageView7),
+            rootview.findViewById<ImageView>(R.id.imageView8),
+            rootview.findViewById<ImageView>(R.id.imageView12),
+            rootview.findViewById<ImageView>(R.id.imageView13),
+            rootview.findViewById<ImageView>(R.id.imageView14),
+        )
+        for(i in imageviewarr.indices){
+            imageviewarr[i].setImageResource(imagearr[i])
+        }
+        for(i in numbertextarr.indices){
+            numbertextarr[i].setText("${i+1}")
+        }
+
+        return rootview
     }
 
 }
